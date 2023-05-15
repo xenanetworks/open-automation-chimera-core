010e687d1c73422713963379946a954db264f671
fpfeng <minifen9@gmail.com>
2023-05-15 12:18:48 +0800

    feat: setup.py works

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ def main() -> None:
     setuptools.setup(
         name="chimera-core",
         description="wait to be fill",
-        packages=setuptools.find_packages(),
-        install_requires=["xoa_driver>=1.0.12", "pydatic", "loguru"],
+        packages=['chimera_core'],
+        install_requires=["xoa_driver>=1.0.12", "pydantic", "loguru"],
     )
+
+if __name__ == '__main__':
+    main()
